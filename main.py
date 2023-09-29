3e80b3573d5b7990802b9e785acfa861a9240de1
MohamadIlyas07 <146089572+MohamadIlyas07@users.noreply.github.com>
2023-09-29 16:57:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,16 @@
+def linearSearchProduct(productList, targetProduct):
+  indices = []
 
-class BankAccount:
-  def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-    self.__account_number = account_number
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
-  def deposit(self, amount):
-    if amount > 0:
-      self.__account_balance += amount
-      # self.__account_balance = self.__account_balance+amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount,
-                                                     self.__account_balance))
-    else:
-      print("Invalid deposit amount.")
-  def withdraw(self, amount):
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      # self.__account_balance = self.__account_balance - amount
-      print("Withdrew ₹{}. New balance: ₹{}".format(amount,
-                                                    self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
-  def display_balance(self):
-    print("Account balance for {} (Account #{}): ₹{}".format(
-        self.__account_holder_name, self.__account_number,
-        self.__account_balance))
-# Create an instance of the BankAccount class
-account = BankAccount(account_number="123456789",
-                      account_holder_name="Hari Prabu",
-                      initial_balance=5000.0)
-# Test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-account.display_balance()
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+
+  return indices
+
+
+# Example usage:
+products = ["shoes", "boot", "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+target2 = 'apple'
+result = linearSearchProduct(products, target)
+print(result)
